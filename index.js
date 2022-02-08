af80a8ade709a215e8a09a9d7d9c8de61a9b48af
Nirajan Acharya <47442286+nacharya01@users.noreply.github.com>
2022-02-08 00:10:17 -0500

    Add files via upload
    Final Changes

---
+++ index.js
@@ -0,0 +1,18 @@
+function openInstagram(){
+    let but= document.getElementById("insta").onclick=function()
+    {
+        location.href="https://www.instagram.com/nirajan__acharya/";
+    }
+}
+function openTwitter(){
+    let linked= document.getElementById("twitter").onclick=function()
+    {
+        location.href="https://www.twitter.com/";
+    }
+}
+function openLinkedIn(){
+    let linked= document.getElementById("linked").onclick=function()
+    {
+        location.href="https://www.linkedin.com/in/nirajan-acharya-59937322a/";
+    }
+}
